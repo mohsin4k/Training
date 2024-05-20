397f1770b7a1301de51e349fabdefcad9ea46bd2
unknown <khanmk2000@gmail.com>
2024-05-19 23:26:54 -0700

    Some topics of javascript

--- newFile.js
+++ newFile.js
@@ -1,7 +1,44 @@
-function come(x){
-    return function (y){
-        return x*y; 
-    }
-}
+// Currying Example
+// function come(x){
+//     return function (y){
+//         return x*y; 
+//     }
+// }
 
-console.log(come(3)(4));
+// console.log(come(3)(4));
+
+
+//Closure Example
+function makeFunc() {
+    const name = "Mozilla";
+    function displayName() {
+      console.log(name);
+    }
+    return displayName;
+  }
+  
+  const myFunc = makeFunc();
+//   myFunc();
+
+
+//This keyword
+function fullName() {
+    return this.firstName + " " + this.lastName;
+  }
+
+//   console.log(fullName());
+  
+
+  const person = {
+    firstName  : "John",
+    lastName   : "Doe",
+    id         : 5566,
+    myFunction : function() {
+      return this.firstName + " " + this.lastName;
+    }
+  };
+
+  let x = person;
+
+  
+  console.log(x.myFunction()); 
